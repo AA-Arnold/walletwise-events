4d9b9837be5001161e80e50702d59dcd28661a86
wills-dev <ejioguvictor6@gmail.com>
2026-05-22 12:18:21 +0100

    added: calendar icon to input

--- src/components/atoms/Input copy 2/Input.tsx
+++ src/components/atoms/Input copy 2/Input.tsx
@@ -1,3 +1,5 @@
+import { Calendar } from "lucide-react";
+
 const Input = ({
   onChange,
   value,
@@ -15,7 +17,7 @@ const Input = ({
   icon?: string;
 }) => {
   return (
-    <div className="w-full sm:h-11 h-10 bg-[#120909] border-[#404040] border px-[14.64px] flex items-center focus-within:border-[#777777] rounded-[11px]">
+    <div className="w-full sm:h-11 h-10 bg-[#120909] border-[#404040] border px-[14.64px] flex items-center focus-within:border-[#777777] rounded-[11px] relative">
       <input
         style={{ fontSize: "16px" }}
         type={type}
@@ -26,6 +28,9 @@ const Input = ({
         className="bg-inherit h-full flex-1 w-full outline-none text-[#C1C1C1] placeholder:text-[#777777] text-sm"
         placeholder={placeholder}
       />
+      {type == "date" && (
+        <Calendar className="absolute right-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400 pointer-events-none" />
+      )}
     </div>
   );
 };
