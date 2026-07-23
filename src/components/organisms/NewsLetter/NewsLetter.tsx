00dd5ccfbc02da34a4ebd4847fd1c9194cd87be2
wills-dev <ejioguvictor6@gmail.com>
2026-07-23 11:23:40 +0100

    added:

--- src/components/organisms/NewsLetter/NewsLetter.tsx
+++ src/components/organisms/NewsLetter/NewsLetter.tsx
@@ -8,36 +8,38 @@ const NewsLetter = () => {
     <section>
       <Container>
         <div className="rounded-[8px] p-4 bg-linear-to-tr from-0% from-[#8963DD] to-100% to-[#03A48B] ">
-          <div className="flex justify-between items-center gap-3.25 flex-wrap">
-            <div className="flex items-center gap-3.25">
+          <div className="flex md:items-center gap-3.25 ">
+            <div className="max-sm:pt-2">
               <Tags />
+            </div>
+            <div className="flex items-center justify-between flex-wrap gap-3.25 flex-1">
               <p className="text-white font-medium max-sm:text-sm">
                 Cheaper tickets, only on the WalletWise app. Download now{" "}
               </p>
-            </div>
-            <div className="rounded-full border border-[#F5F5F5] text-white flex items-center overflow-hidden">
-              <a
-                href="https://apps.apple.com/us/app/walletwise-seamless-payment/id6744754064"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center p-3 gap-2 border-r border-[#F5F5F5] bg-white/15"
-              >
-                <span className="font-medium sm:text-sm text-xs">
-                  App Store
-                </span>
-                <PlayStoreIcon />
-              </a>
-              <a
-                href="https://play.google.com/store/apps/details?id=com.finance.walletwise.app&pcampaignid=web_share"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center p-3 gap-2 bg-white/15"
-              >
-                <span className="font-medium sm:text-sm text-xs">
-                  Google Play
-                </span>
-                <AppleIcon />
-              </a>
+              <div className="rounded-full border border-[#F5F5F5] text-white flex items-center overflow-hidden">
+                <a
+                  href="https://apps.apple.com/us/app/walletwise-seamless-payment/id6744754064"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center p-3 gap-2 border-r border-[#F5F5F5] bg-white/15"
+                >
+                  <span className="font-medium sm:text-sm text-xs">
+                    App Store
+                  </span>
+                  <PlayStoreIcon />
+                </a>
+                <a
+                  href="https://play.google.com/store/apps/details?id=com.finance.walletwise.app&pcampaignid=web_share"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center p-3 gap-2 bg-white/15"
+                >
+                  <span className="font-medium sm:text-sm text-xs">
+                    Google Play
+                  </span>
+                  <AppleIcon />
+                </a>
+              </div>
             </div>
           </div>
         </div>
