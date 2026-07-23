474fcd9da18de0642f796ded91df502a9c7c5c7b
wills-dev <ejioguvictor6@gmail.com>
2026-07-23 10:26:17 +0100

    added:

--- src/components/organisms/NewsLetter/NewsLetter.tsx
+++ src/components/organisms/NewsLetter/NewsLetter.tsx
@@ -1,42 +1,44 @@
 import Container from "@/components/atom/Container/Container";
-import OurSocials from "@/components/molecules/OurSocials/OurSocials";
-import Image from "next/image";
+import AppleIcon from "@/components/atom/icons/AppleIcon";
+import PlayStoreIcon from "@/components/atom/icons/PlayStoreIcon";
+import Tags from "@/components/atom/icons/Tags";
 
 const NewsLetter = () => {
   return (
     <section>
       <Container>
-        <div className="rounded-[24px] py-10 sm:px-8 px-6 bg-[#A53D41] relative">
-          <div className="absolute w-full h-full top-0 left-0 right-0 bottom-0">
-            <Image
-              src={"/assets/images/david.png"}
-              alt="david"
-              width={1280}
-              height={148}
-              className="w-full h-full"
-            />
-          </div>
-          <div className="flex items-center justify-between flex-wrap gap-10">
-            <div className="">
-              <h6 className="sm:text-[28px] text-2xl font-semibold text-white">
-                New events drop regularly
-              </h6>
-              <p className="text-white/60 sm:text-xl text-lg font-medium">
-                Be the first to know
+        <div className="rounded-[8px] p-4 bg-linear-to-tr from-0% from-[#8963DD] to-100% to-[#03A48B] ">
+          <div className="flex justify-between items-center gap-3.25 flex-wrap">
+            <div className="flex items-center gap-3.25">
+              <Tags />
+              <p className="text-white font-medium max-sm:text-sm">
+                Cheaper tickets, only on the WalletWise app. Download now{" "}
               </p>
             </div>
-            <OurSocials />
-          </div>
-          <div className="absolute top-0 left-0 bottom-0 right-0 flex items-center justify-between flex-wrap gap-10 sm:px-8 px-6 z-10">
-            <div className="">
-              <h6 className="sm:text-[28px] text-2xl font-semibold text-white">
-                New events drop regularly
-              </h6>
-              <p className="text-white/60 sm:text-xl text-lg font-medium">
-                Be the first to know
-              </p>
+            <div className="rounded-full border border-[#F5F5F5] text-white flex items-center overflow-hidden">
+              <a
+                href="https://apps.apple.com/us/app/walletwise-seamless-payment/id6744754064"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center p-3 gap-2 border-r border-[#F5F5F5] bg-white/15"
+              >
+                <span className="font-medium sm:text-sm text-xs">
+                  App Store
+                </span>
+                <PlayStoreIcon />
+              </a>
+              <a
+                href="https://play.google.com/store/apps/details?id=com.finance.walletwise.app&pcampaignid=web_share"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center p-3 gap-2 bg-white/15"
+              >
+                <span className="font-medium sm:text-sm text-xs">
+                  Google Play
+                </span>
+                <AppleIcon />
+              </a>
             </div>
-            <OurSocials />
           </div>
         </div>
       </Container>
